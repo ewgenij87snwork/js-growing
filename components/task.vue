d4baae11646d555caf7b313053cad212a21841ed
ewgenij87s <50303121+ewgenij87s@users.noreply.github.com>
2019-08-22 21:32:36 +0200

    task props not yet

---
+++ components/task.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="task__container">
+		<div class="task" v-for="item in tasks" :key="item.name">
+			<div class="decision">
+				<!-- <button v-on:click="{{ item.func }}">{{ item.name }}</button> -->
+				<!-- <p class="answer" :id="{{ item.name }}">Press button to see result</p> -->
+
+				<!-- Как через пропсы передавать функцию на выполнение?
+				Почему не могу передать в id пропс? :id="{{ item.name }}
+				Почему не могу передать в кнопку название обработчика? v-on:click="{{ item.func }}"> -->
+			</div>
+			<pre><code><b>{{ item.name }}</b> function() {
+		{{ item.codeExemple }}
+			}</code></pre>
+			<button v-on:click="show = !show" class="subButton">Show method description</button>
+				<div class="methodDescription">
+					<transition name="fade" mode="out-in">
+						<p v-show="show">Метод <b>{{ item.name }}</b> {{ item.description }}</p>
+					</transition>
+				</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  props: {
+	tasks: {
+		type:Array,
+		required: true
+	}
+  },
+  data: function() {
+  	return {
+  		show: false,
+  	}
+  },
+  methods: {
+  	item.func()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- pages/cheatsheet/arrayz.vue
+++ pages/cheatsheet/arrayz.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="container">
 		<div class="titlePage"><h1>Array Methods</h1></div>
-
+		<task
+		v-for="task in tasks"
+		v-bind:func="task.func"
+		v-bind:title="task.title"
+		v-bind:codeExemple="task.codeExemple"
+		v-bind:description="task.description"
+		v-bind:key="task.id"
+		/>
 		<div class="task">
 			<div class="decision">
 				<button v-on:click="concat">concat</button>
@@ -9,7 +16,6 @@
 			</div>
 			<pre><code><b>concat:</b> function() {
 		var alpha = ['a', 'b', 'c'];
-		var alphaNumeric = alpha.concat(1, [2, 3]);
 		document.getElementById("concat").innerHTML = alpha.concat(1, [2, 3]);
 	}</code></pre>
 			<button v-on:click="show = !show" class="subButton">Show method description</button>
@@ -29,25 +35,44 @@
 		document.getElementById("every").innerHTML = [12, 54, 18, 130, 44].every(elem => elem >= 10);
 	}</code></pre>
 			<button v-on:click="show = !show" class="subButton">Show method description</button>
-			<div class="methodDescription"><p v-show="show">Метод <b>every()</b> проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции.</p></div>
+			<div class="methodDescription">
+				<transition name="fade" mode="out-in">
+					<p v-show="show">Метод <b>every()</b> проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции.</p>
+				</transition>
+			</div>
 		</div>
 
 
 	</div>
 </template>
 <script>
+import	task from "@/components/task";
 	export default {
 		name: 'arrayz',
 		data: function() {
 			return {
 				show: false,
+				tasks: [
+					{
+						id: 1,
+						func: 'concat',
+						title: 'concat',
+						codeExemple: 'bla bla',
+						description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla est quaerat aut amet non, cupiditate expedita. Distinctio quidem voluptatibus vero reprehenderit, consequatur corrupti fugiat sequi eius quos, tempora corporis illo optio vel quia quisquam deleniti autem. Quia, maiores, esse.',
+					},
+					{
+						id: 2,
+						func: '2concat',
+						title: '2concat',
+						codeExemple: '2bla bla',
+						description: '2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla est quaerat aut amet non, cupiditate expedita. Distinctio quidem voluptatibus vero reprehenderit, consequatur corrupti fugiat sequi eius quos, tempora corporis illo optio vel quia quisquam deleniti autem. Quia, maiores, esse.',
+					},
+				]
 			}
 		},
 		methods: {
 			concat: function() {
 				var alpha = ['a', 'b', 'c'];
-
-				var alphaNumeric = alpha.concat(1, [2, 3]);
 				document.getElementById("concat").innerHTML = alpha.concat(1, [2, 3]);
 			},	
 			every: function() {
